50772710a3b08bb910d15833e7104270957c2cda
luzhipeng <luzhipeng@healthych.com>
2021-04-07 16:23:22 +0800

    1.7.2

--- src/main/java/com/apigcc/maven/ApigccMojo.java
+++ src/main/java/com/apigcc/maven/ApigccMojo.java
@@ -2,13 +2,22 @@ package com.apigcc.maven;
 
 import com.github.apigcc.core.Apigcc;
 import com.github.apigcc.core.Context;
+import com.github.apigcc.core.DirModule;
+import com.github.apigcc.core.ExtConfig;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * generate rest doc with apigcc
@@ -37,18 +46,24 @@ public class ApigccMojo extends AbstractMojo {
     String version;
     @Parameter
     String css;
+    //    @Parameter
+//    String urlPrefix;
     @Parameter
-    String urlPrefix;
+    String extYamlPath;
 
     @Override
     public void execute() {
-        if(getPluginContext().containsKey("project") && getPluginContext().get("project") instanceof MavenProject){
+        if(getPluginContext().containsKey("project") && getPluginContext().get("project") instanceof MavenProject) {
             project = (MavenProject) getPluginContext().get("project");
-            build();
+            try {
+                build();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private void build(){
+    private void build() throws IOException {
         Context context = new Context();
         if (source != null) {
             for (String dir : source.split(",")) {
@@ -86,22 +101,46 @@ public class ApigccMojo extends AbstractMojo {
         } else if (project.getDescription()!=null) {
             context.setDescription(project.getDescription());
         }
-        if (version != null){
+        if (version != null) {
             context.setVersion(version);
-        } else if (project.getVersion()!=null){
+        } else if (project.getVersion() != null) {
             context.setVersion(project.getVersion());
         }
         if (css != null) {
             context.setCss(css);
         }
-        if (urlPrefix != null) {
-            context.setUrlPrefix(urlPrefix);
+//        if (urlPrefix != null) {
+//            context.setUrlPrefix(urlPrefix);
+//        }
+        if (extYamlPath != null) {
+            context.setExtYamlPath(extYamlPath);
         }
 
-        Apigcc apigcc = new Apigcc(context);
-        apigcc.parse();
-        apigcc.render();
+//        Apigcc apigcc = new Apigcc(context);
+//        apigcc.parse();
+//        apigcc.render();
 
+        Yaml yaml = new Yaml(new Constructor(ExtConfig.class));
+        ExtConfig extConfig = (ExtConfig) yaml.load(new FileInputStream(extYamlPath));
+        List<Path> dirList = Files.walk(Paths.get(extConfig.getRootDir()), extConfig.getMaxDepth())
+                .filter(p -> extConfig.getModules().stream().anyMatch(m -> m.getDirName().equals(p.getFileName().toString()))).collect(Collectors.toList());
+        for (Path path : dirList) {
+            String name = path.getFileName().toString();
+            DirModule dirModule = extConfig.getModules().stream().filter(m -> m.getDirName().equals(name)).findFirst().get();
+            Context c = new Context();
+            c.setId(name);
+            c.setName(name);
+            c.setUrlPrefix(dirModule.getUrlPrefix());
+            c.addSource(path);
+            c.addDependency(path);
+            extConfig.getJars().forEach(s -> c.addJar(Paths.get(s)));
+            c.setBuildPath(Paths.get(extConfig.getBuildPath()));
+
+            Apigcc apigcc = new Apigcc(c);
+            apigcc.setExtConfig(extConfig);
+            apigcc.parse();
+            apigcc.render();
+        }
     }
 
     private MavenProject findParent(MavenProject mp){
